fd863aa2f550e2293322aecabc2b76a6e424426c
Lucas Switzer <lucswitz@gmail.com>
2017-02-14 00:29:13 -0500

    Cleaned Code and added precision to get_depth
    - changed get_depth() to return the 16bit float array rather than the
    converted uint8 array
    
    - removed unused imports and whitespace

--- stereo/depth_map_test.py
+++ stereo/depth_map_test.py
@@ -1,23 +1,29 @@
 import cv2
 import numpy
-from matplotlib import pyplot as plt
 
 
 def get_depth(img_1, img_2):
+    """ Takes 2 images captured from a stereo vision rig and computes the disparities between the image.
 
-
+    Args:
+        img_1 - first image
+        img_2 - second image
+    
+    Returns:
+        A numpy array containing 16 bit fixed point values representing a disparty map
+    """
     img_1_grey = cv2.cvtColor(img_1, cv2.COLOR_BGR2GRAY)
     img_2_grey = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
 
     stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+
     disparity = stereo.compute(img_1_grey, img_2_grey)
 
-    return disparity.astype("uint8")
+    return disparity
 
 def loop():
     vc_1 = cv2.VideoCapture(0)
     vc_2 = cv2.VideoCapture(1)
-
 
     vc_1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     vc_1.set(cv2.CAP_PROP_FRAME_HEIGHT, 420)
@@ -32,9 +38,7 @@ def loop():
 
     while True:
         _, img_1 = vc_1.read()
-
         _, img_2 = vc_2.read()
-
 
         key = cv2.waitKey(1)
         if key == 27:
@@ -43,12 +47,10 @@ def loop():
         if img_1 is not None and img_2 is not None:
             depth_image = get_depth(img_1,img_2)
             if depth_image is not None:
-                cv2.imshow("depth", depth_image)
+                cv2.imshow("depth", depth_image.astype("uint8"))
 
             cv2.imshow("left", img_1)
             cv2.imshow("right", img_2)
-
-
 
 if __name__ == "__main__":
     loop()
