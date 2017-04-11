5463de7f0ede81f6d8d15c8f779e6f9ea8c1cdaf
Ning Wang <nw265@cornell.edu>
2017-04-11 17:10:51 -0400

    added updated slam file

--- feature detection/slam.py
+++ feature detection/slam.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy
 
+last_cap = None
+
 calib_left_mat = numpy.array(
     [[ 541.11082257,    0.        ,  324.08789568],
      [   0.        ,  541.11082257,  222.59189031],
@@ -14,29 +16,80 @@ calib_left_dist = numpy.array(
 calib_right_dist = numpy.array(
     [[ 0.25088046, -1.03260309,  0.        ,  0.        ,  1.41224436]])
 
+cp_x = calib_left_mat[0][2]
+cp_y = calib_left_mat[1][2]
+fx = calib_left_mat[0][0]
+fy = calib_left_mat[1][1]
 
-max_features = 10
+depth_string_pixel = ""
+depth_string_cm = ""
+max_features = 50
+
+point_3d_string = ""
+def image_plane_to_world_space(pixel_x, pixel_y, image_center_x, image_center_y, focal_length_x, focal_length_y, depth):
+    x = ((pixel_x - image_center_x)*depth_pixel_to_cm(depth)) / (focal_length_x)
+    y = ((pixel_y - image_center_y)*depth_pixel_to_cm(depth)) / (focal_length_y)
+    return [x,y,depth_pixel_to_cm(depth)]
+
+
+def decompose_keypoint(keypoint):
+    keypoint_out = []
+
+    keypoint_out.append(float(keypoint.pt[0]))
+    keypoint_out.append(float(keypoint.pt[1]))
+
+    return keypoint_out
+
+def get_similar_feature_keypoints(keypoints, desc1, desc2):
+    keypoints_out = []
+    desc_out = []
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(desc1,desc2, k = 2)
+
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            if m.trainIdx < len(keypoints):
+                keypoints_out.append(decompose_keypoint(keypoints[m.trainIdx]))
+            else:
+                 keypoints_out.append(decompose_keypoint(keypoints[m.queryIdx]))
+            desc_out.append([m])
+    
+    return (numpy.asarray(keypoints_out), desc_out)
 
 def slam(lf, rf):
-    stereo = cv2.StereoSGBM_create(minDisparity = 16,
-        numDisparities = 96,
-        blockSize = 16,
+    global last_cap
+    global disparity
+    stereo = cv2.StereoSGBM_create(minDisparity = 25,
+        numDisparities = 64,
+        blockSize = 9,
         P1 = 8*3*3*2,
         P2 = 32*3*3*2,
         disp12MaxDiff = 1,
-        uniquenessRatio = 10,
-        speckleWindowSize = 100,
-        speckleRange = 32,
-        mode = cv2.STEREO_SGBM_MODE_HH)
+        preFilterCap = 63,
+        uniquenessRatio = 7,
+        speckleWindowSize = 50,
+        speckleRange = 1)         
     
     luf = cv2.undistort(lf, calib_left_mat, calib_left_dist)
-    ruf = cv2.undistort(lf, calib_right_mat, calib_right_dist)
-    disparity = stereo.compute(luf, ruf)
-        
-    detector = cv2.ORB_create()
-    lkp, ldesc = detector.detectAndCompute(luf, None)
-    rkp, rdesc = detector.detectAndCompute(ruf, None)
+    ruf = cv2.undistort(rf, calib_right_mat, calib_right_dist)
 
+    lf_g = cv2.cvtColor(luf, cv2.COLOR_BGR2GRAY)
+    rf_g = cv2.cvtColor(ruf, cv2.COLOR_BGR2GRAY)
+
+    disparity = stereo.compute(lf_g, rf_g)
+
+    display_norm = numpy.zeros(disparity.shape, dtype = "uint8")
+    cv2.normalize(disparity,display_norm, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_8U)
+
+    cv2.putText(display_norm,depth_string_pixel,(0,20), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,0),1,cv2.LINE_AA)
+    cv2.putText(display_norm,depth_string_cm,(0,50), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,0),1,cv2.LINE_AA)
+    cv2.imshow("Disparity", display_norm)
+
+    detector = cv2.ORB_create()
+    #detect features in this frame
+    lkp, ldesc = detector.detectAndCompute(luf, None)
+
+    
     if len(lkp) > max_features:
         kpde = list(zip(lkp, ldesc))
         lkpde = sorted(kpde, key=lambda kd: kd[0].response, reverse=True)
@@ -45,42 +98,77 @@ def slam(lf, rf):
         lkp = list(lkp)
         ldesc = numpy.asarray(ldesc)
 
-    if len(rkp) > max_features:
-        kpde = list(zip(rkp, rdesc))
-        rkpde = sorted(kpde, key=lambda kd: kd[0].response, reverse=True)
-        rkpde = rkpde[:max_features]
-        rkp, rdesc = list(zip(*rkpde))
-        rkp = list(rkp)
-        rdesc = numpy.asarray(rdesc)
+        
+    keypoints = []
+    points_3d = []
+
+    if last_cap is None:
+        for k in lkp:
+            keypoints.append(decompose_keypoint(k))
+    else:
+        keypoints, descs = get_similar_feature_keypoints(lkp,ldesc,last_cap[2])        
     
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(ldesc,rdesc, k=2)
+    luf_copy = luf.copy()
+    for x,y in keypoints:
+        depth = disparity[int(y)][int(x)]
+        point_3d = image_plane_to_world_space(x, y, cp_x, cp_y, fx, fy, depth)
+        points_3d.append(point_3d)
 
-    # Apply ratio test
-    good = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append([m])
 
-    # cv2.drawMatchesKnn expects list of lists as matches.
-    img3 = cv2.drawMatchesKnn(luf,lkp,ruf,rkp,good,outImg = None,flags=2)
+        r = (255.0 / 150.0) * abs(point_3d[0]) #the closer to center x, the greater r will be
+        g = (255.0 / 113) * abs(point_3d[1]) # the closer to center y, the greater g will be 
+        b = (255.0 / 1024.0)*(depth) # the closer, the greater b will be 
 
-    return img3
+        cv2.circle(luf_copy,(int(x),int(y)),2,(int(r),int(b),int(g)),5)
 
+    cv2.putText(luf_copy,point_3d_string,(0,20), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,0),1,cv2.LINE_AA)
+    cv2.imshow("Features in 3D", luf_copy)
+   
+    if(len(points_3d) > 2  and len(keypoints) > 2):
+        ret, rvec, tvec = cv2.solvePnP(numpy.asarray(points_3d), numpy.asarray(keypoints), calib_left_mat, distCoeffs= None)
+
+        if last_cap is not None:
+            match_frame = cv2.drawMatchesKnn(luf,lkp,last_cap[0],last_cap[1],descs, outImg = None,flags=2)
+            cv2.putText(match_frame,str(tvec),(0,20), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,0),1,cv2.LINE_AA)
+            cv2.imshow("Matches", match_frame)
+
+        last_cap = [luf, lkp, ldesc, ret, rvec, tvec]
+
+        
+
+def callback_depth(event, x, y, flags, params):
+     global depth_string_pixel
+     global depth_string_cm
+     
+     depth_pixel = disparity[y][x]
+     depth_string_pixel = str(depth_pixel) + " px"
+     depth_string_cm = str(depth_pixel_to_cm(depth_pixel)) + " cm"
+     
+def callback_3d_points(event, x,y,flags, params):
+    global point_3d_string
+    
+    depth = disparity[y][x]
+    point = image_plane_to_world_space(x, y, cp_x, cp_y, fx, fy, depth)
+    point_3d_string = str((point)) + " (cm)"
+    
+def depth_pixel_to_cm(pixel_val):
+    return 496.8272 + pixel_val*-0.35114
+    
 if __name__=="__main__":
-    lvc = cv2.VideoCapture("left.avi")
-    rvc = cv2.VideoCapture("right.avi")
+    lvc = cv2.VideoCapture(1)
+    rvc = cv2.VideoCapture(0)
+    
+    cv2.namedWindow("Disparity")
+    cv2.setMouseCallback("Disparity", callback_depth)
+    
+    cv2.namedWindow("Features in 3D")
+    cv2.setMouseCallback("Features in 3D", callback_3d_points)
     
     while True:
         lret, lf = lvc.read()
         rret, rf = rvc.read()
         
-        if lret and rret:
-            cv2.imshow("Left", lf)
-            cv2.imshow("Right", rf)
-            
-            res = slam(lf, rf)
-            cv2.imshow("Slam", res)
+        res = slam(lf, rf)
     
         k = cv2.waitKey(1)
         if k == 27:
